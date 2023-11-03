28390b6547dbfe1669b70a17f2e3fee87d40851b
matyiii <mezofimate@gmail.com>
2023-11-03 19:16:51 +0100

    added Activity Seeder

--- API/Program.cs
+++ API/Program.cs
@@ -33,7 +33,9 @@ var services = scope.ServiceProvider;
 try
 {
 	var context = services.GetRequiredService<DataContext>();
-	context.Database.Migrate();
+
+	await context.Database.MigrateAsync();
+	await Seed.SeedData(context);
 }
 catch (Exception ex)
 {
