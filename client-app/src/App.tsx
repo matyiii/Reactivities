78242bc03f36c1e0140a5fb7171fa401bf030929
matyiii <mezofimate@gmail.com>
2023-11-05 16:43:25 +0100

    refactor(frontend): added Semantic UI

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 const App = () => {
     /* States */
@@ -8,19 +9,19 @@ const App = () => {
 
     /* Effects */
     useEffect(() => {
-        axios
-            .get('http://localhost:5000/api/activities')
-            .then((response) => setActivities(response.data));
+        axios.get('http://localhost:5000/api/activities').then((response) => {
+            setActivities(response.data);
+        });
     }, []);
 
     return (
         <>
-            <h1>Reactivities</h1>
-            <ul>
+            <Header as="h1" icon="users" content="Reactivites" />
+            <List>
                 {activities.map((activity: any) => (
-                    <li key={activity.id}>{activity.title}</li>
+                    <List.Item key={activity.id}>{activity.title}</List.Item>
                 ))}
-            </ul>
+            </List>
         </>
     );
 };
