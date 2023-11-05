a778c4adca96d3d3be3e119ef5125417364d9046
matyiii <mezofimate@gmail.com>
2023-11-05 11:35:33 +0100

    frontend: added Axios

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,9 +1,28 @@
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <h1>Reactivities</h1>
-  )
-}
+const App = () => {
+    /* States */
+    const [activities, setActivities] = useState([]);
 
-export default App
+    /* Effects */
+    useEffect(() => {
+        axios
+            .get('http://localhost:5000/api/activities')
+            .then((response) => setActivities(response.data));
+    }, []);
+
+    return (
+        <>
+            <h1>Reactivities</h1>
+            <ul>
+                {activities.map((activity: any) => (
+                    <li key={activity.id}>{activity.title}</li>
+                ))}
+            </ul>
+        </>
+    );
+};
+
+export default App;
