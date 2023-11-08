17c0b3a67dc46fa7798b57946d20d3325d7adabb
matyiii <mezofimate@gmail.com>
2023-11-08 19:04:53 +0100

    backend: added MediatoR

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,4 +1,6 @@
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
@@ -7,22 +9,22 @@ namespace API.Controllers
 {
 	public class ActivitiesController : BaseApiController
 	{
-		private readonly DataContext context;
-		public ActivitiesController(DataContext context)
+		private readonly IMediator mediator;
+		public ActivitiesController(IMediator mediator)
 		{
-			this.context = context;
+			this.mediator = mediator;
 		}
 
 		[HttpGet] //api/activites
 		public async Task<ActionResult<List<Activity>>> GetActivities()
 		{
-			return await context.Activities.ToListAsync();
+			return await mediator.Send(new List.Query());
 		}
 
 		[HttpGet("{id}")] //api/activites/id
 		public async Task<ActionResult<Activity>> GetActivityById(Guid id)
 		{
-			return await context.Activities.FirstOrDefaultAsync(x => x.Id == id);
+			return null;
 		}
 	}
 }
